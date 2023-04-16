98991d036588e217ba97278099e9577983851561
dsims12 <diogo.sanchessimoes@student.kuleuven.be>
2023-04-16 16:48:21 +0200

    Exercise 5 done

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,9 @@
-I really like data visualisation!
+<style>
+    p {
+        color: steelblue;
+        font-size: 200%;
+    }
+</style>
+
+<p>I <span style="color:#f00">really</span> like data visualisation!</p>
+
