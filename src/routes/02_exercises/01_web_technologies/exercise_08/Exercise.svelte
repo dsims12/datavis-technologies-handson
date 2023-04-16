717010dca2c818c50192d34675cab6f3d6910c5a
dsims12 <diogo.sanchessimoes@student.kuleuven.be>
2023-04-16 17:24:45 +0200

    Exercise 8 done

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,11 @@
+<script>
+    const favorite_things = ['Books', 'Succession', 'Music'];
+    const student = { name: 'Diogo', fav_things: favorite_things};
+</script>
+
+<p>My name is {student.name} and  my favourite things are: </p>
+<p> 
+    <li>{student.fav_things[0]}</li> 
+    <li>{student.fav_things[1]}</li> 
+    <li>{student.fav_things[2]}</li>
+</p>
